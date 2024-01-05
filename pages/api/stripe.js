805d59ed1d573d5e55b6d81cad8e2fbff1095909
eslam reda <eslam.reda.div@gmail.com>
2024-01-05 10:28:32 +0200

    test

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -11,9 +11,9 @@ export default async function handler(req, res) {
         mode: 'payment',
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
-        // shipping_options: [
-        //   { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
-        // ],
+        shipping_options: [
+          { shipping_rate: 'shr_1OV8k9H9RHdRyF0gKoIZ2JYF' },
+        ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
           const newImage = img.replace('image-', 'https://cdn.sanity.io/images/azfdx77a/production/').replace('-webp', '.webp');
